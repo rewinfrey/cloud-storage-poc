12a917b748b72761a0f046a67dbaf0ee03a947bd
Rick Winfrey <rewinfrey@github.com>
2020-07-13 17:13:08 -0700

    Add timings metrics

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -12,120 +13,149 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
-func exitErrorf(msg string, args ...interface{}) {
+func exitErrorf(exit bool, msg string, args ...interface{}) {
 	fmt.Fprintf(os.Stderr, msg+"\n", args...)
-	os.Exit(1)
-}
-
-func listAllBuckets(svc *s3.S3) {
-	result, err := svc.ListBuckets(nil)
-	if err != nil {
-		exitErrorf("Unable to list buckets, %v", err)
-	}
-
-	fmt.Println("Buckets:")
-
-	for _, b := range result.Buckets {
-		fmt.Printf("* %s created on %s\n", aws.StringValue(b.Name), aws.TimeValue(b.CreationDate))
+	if exit {
+		os.Exit(1)
 	}
 }
 
-func createBucket(svc *s3.S3, name string) {
-	_, err := svc.CreateBucket(&s3.CreateBucketInput{
-		Bucket: aws.String(name),
-	})
-	if err != nil {
-		exitErrorf("Unable to create bucket %q, %v", name, err)
+func listAllBuckets(svc *s3.S3) func() {
+	return func() {
+		result, err := svc.ListBuckets(nil)
+		if err != nil {
+			exitErrorf(true, "Unable to list buckets, %v", err)
+		}
+
+		fmt.Println("Buckets:")
+
+		for _, b := range result.Buckets {
+			fmt.Printf("* %s created on %s\n", aws.StringValue(b.Name), aws.TimeValue(b.CreationDate))
+		}
 	}
-
-	// Wait until bucket is created before finishing
-	fmt.Printf("Waiting for bucket %q to be created...\n", name)
-
-	err = svc.WaitUntilBucketExists(&s3.HeadBucketInput{
-		Bucket: aws.String(name),
-	})
-	if err != nil {
-		exitErrorf("Error occurred while waiting for bucket to be created, %v", name)
-	}
-
-	fmt.Printf("Bucket %q successfully created\n", name)
 }
 
-func writeBucket(uploader *s3manager.Uploader, bucket string, filename string) {
-	file, err := os.Open(filename)
-	if err != nil {
-		exitErrorf("Unable to open file %q, %v", err)
-	}
+func createBucket(svc *s3.S3, name string) func() {
+	return func() {
+		_, err := svc.CreateBucket(&s3.CreateBucketInput{
+			Bucket: aws.String(name),
+		})
+		if err != nil {
+			exitErrorf(true, "Unable to create bucket %q, %v", name, err)
+		}
 
-	_, err = uploader.Upload(&s3manager.UploadInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(filename),
-		Body:   file,
-	})
-	if err != nil {
-		// Print the error and exit.
-		exitErrorf("Unable to upload %q to %q, %v", filename, bucket, err)
-	}
+		// Wait until bucket is created before finishing
+		fmt.Printf("Waiting for bucket %q to be created...\n", name)
 
-	fmt.Printf("Successfully uploaded %q to %q\n", filename, bucket)
+		err = svc.WaitUntilBucketExists(&s3.HeadBucketInput{
+			Bucket: aws.String(name),
+		})
+		if err != nil {
+			exitErrorf(true, "Error occurred while waiting for bucket to be created, %v", name)
+		}
+
+		fmt.Printf("Bucket %q successfully created\n", name)
+	}
 }
 
-func readBucket(downloader *s3manager.Downloader, bucket string, filename string) {
-	file, err := os.Create("tmp/" + filename)
-	if err != nil {
-		exitErrorf("Unable to open file %q, %v", filename, err)
-	}
-	defer file.Close()
+func writeBucket(uploader *s3manager.Uploader, bucket string, filename string) func() {
+	return func() {
+		file, err := os.Open(filename)
+		if err != nil {
+			exitErrorf(true, "Unable to open file %q, %v", err)
+		}
 
-	numBytes, err := downloader.Download(file,
-		&s3.GetObjectInput{
+		_, err = uploader.Upload(&s3manager.UploadInput{
 			Bucket: aws.String(bucket),
 			Key:    aws.String(filename),
+			Body:   file,
 		})
-	if err != nil {
-		exitErrorf("Unable to download item %q, %v", filename, err)
-	}
+		if err != nil {
+			// Print the error and exit.
+			exitErrorf(true, "Unable to upload %q to %q, %v", filename, bucket, err)
+		}
 
-	fmt.Println("Downloaded", file.Name(), numBytes, "bytes")
-}
-
-func listBucket(svc *s3.S3, bucket string) {
-	resp, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(bucket)})
-	if err != nil {
-		exitErrorf("Unable to list items in bucket %q, %v", bucket, err)
-	}
-
-	for _, item := range resp.Contents {
-		fmt.Println("Name:         ", *item.Key)
-		fmt.Println("Last modified:", *item.LastModified)
-		fmt.Println("Size:         ", *item.Size)
-		fmt.Println("Storage class:", *item.StorageClass)
-		fmt.Println("")
+		fmt.Printf("Successfully uploaded %q to %q\n", filename, bucket)
 	}
 }
 
-func deleteBucket(svc *s3.S3, bucket string) {
-	iter := s3manager.NewDeleteListIterator(svc, &s3.ListObjectsInput{
-		Bucket: aws.String(bucket),
-	})
+func readBucket(downloader *s3manager.Downloader, bucket string, filename string) func() {
+	return func() {
+		file, err := os.Create("tmp/" + filename)
+		if err != nil {
+			exitErrorf(true, "Unable to open file %q, %v", filename, err)
+		}
+		defer file.Close()
 
-	if err := s3manager.NewBatchDeleteWithClient(svc).Delete(aws.BackgroundContext(), iter); err != nil {
-		exitErrorf("Unable to delete objects from bucket %q, %v", bucket, err)
+		numBytes, err := downloader.Download(file,
+			&s3.GetObjectInput{
+				Bucket: aws.String(bucket),
+				Key:    aws.String(filename),
+			})
+		if err != nil {
+			exitErrorf(true, "Unable to download item %q, %v", filename, err)
+		}
+
+		fmt.Println("Downloaded", file.Name(), numBytes, "bytes")
 	}
+}
 
-	_, err := svc.DeleteBucket(&s3.DeleteBucketInput{
-		Bucket: aws.String(bucket),
-	})
-	if err != nil {
-		exitErrorf("Unable to delete bucket %q, %v", bucket, err)
+func listBucket(svc *s3.S3, bucket string) func() {
+	return func() {
+		resp, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(bucket)})
+		if err != nil {
+			exitErrorf(true, "Unable to list items in bucket %q, %v", bucket, err)
+		}
+
+		for _, item := range resp.Contents {
+			fmt.Println("Name:         ", *item.Key)
+			fmt.Println("Last modified:", *item.LastModified)
+			fmt.Println("Size:         ", *item.Size)
+			fmt.Println("Storage class:", *item.StorageClass)
+			fmt.Println("")
+		}
 	}
+}
 
-	// Wait until bucket is deleted before finishing
-	fmt.Printf("Waiting for bucket %q to be deleted...\n", bucket)
+func deleteBucketItems(svc *s3.S3, bucket string) func() {
+	return func() {
+		iter := s3manager.NewDeleteListIterator(svc, &s3.ListObjectsInput{
+			Bucket: aws.String(bucket),
+		})
 
-	err = svc.WaitUntilBucketNotExists(&s3.HeadBucketInput{
-		Bucket: aws.String(bucket),
-	})
+		if err := s3manager.NewBatchDeleteWithClient(svc).Delete(aws.BackgroundContext(), iter); err != nil {
+			exitErrorf(false, "Unable to delete objects from bucket %q, %v", bucket, err)
+		}
+	}
+}
+
+func deleteBucket(svc *s3.S3, bucket string) func() {
+	return func() {
+		_, err := svc.DeleteBucket(&s3.DeleteBucketInput{
+			Bucket: aws.String(bucket),
+		})
+		if err != nil {
+			exitErrorf(false, "Unable to delete bucket %q, %v", bucket, err)
+		}
+
+		// Wait until bucket is deleted before finishing
+		fmt.Printf("Waiting for bucket %q to be deleted...\n", bucket)
+
+		err = svc.WaitUntilBucketNotExists(&s3.HeadBucketInput{
+			Bucket: aws.String(bucket),
+		})
+	}
+}
+
+func withTiming(op func(), opName string) {
+	start := time.Now()
+	defer func() {
+		fmt.Println("\n\n*********************")
+		fmt.Printf("%s duration: %v\n", opName, time.Since(start))
+		fmt.Println("*********************")
+	}()
+	fmt.Println("\n\n\n" + opName + ":")
+	op()
 }
 
 func main() {
@@ -134,7 +164,7 @@ func main() {
 		Region: aws.String("us-west-2")},
 	)
 	if err != nil {
-		exitErrorf("Unable to configure new Amazon session, %v", err)
+		exitErrorf(true, "Unable to configure new Amazon session, %v", err)
 		return
 	}
 
@@ -149,26 +179,27 @@ func main() {
 	name := "example." + strconv.Itoa(rand.Int())
 
 	// Clean up previous runs (if needed).
-	deleteBucket(svc, name)
+	withTiming(deleteBucketItems(svc, name), "deleteBucketItems")
+	withTiming(deleteBucket(svc, name), "deleteBucket")
 
 	// Create the bucket.
-	createBucket(svc, name)
+	withTiming(createBucket(svc, name), "createBucket")
 
 	// Write files to the bucket.
-	writeBucket(uploader, name, "tree")
-	writeBucket(uploader, name, "blob1")
-	writeBucket(uploader, name, "blob2")
-	writeBucket(uploader, name, "index")
+	withTiming(writeBucket(uploader, name, "tree"), "writeFile")
+	withTiming(writeBucket(uploader, name, "blob1"), "writeFile")
+	withTiming(writeBucket(uploader, name, "blob2"), "writeFile")
+	withTiming(writeBucket(uploader, name, "index"), "writeFile")
 
 	// Read files from the bucket.
-	readBucket(downloader, name, "tree")
-	readBucket(downloader, name, "blob1")
-	readBucket(downloader, name, "blob2")
-	readBucket(downloader, name, "index")
+	withTiming(readBucket(downloader, name, "tree"), "readFile")
+	withTiming(readBucket(downloader, name, "blob1"), "readFile")
+	withTiming(readBucket(downloader, name, "blob2"), "readFile")
+	withTiming(readBucket(downloader, name, "index"), "readFile")
 
 	// List all buckets.
-	listAllBuckets(svc)
+	withTiming(listAllBuckets(svc), "listAllBuckets")
 
 	// List all within a single bucket.
-	listBucket(svc, name)
+	withTiming(listBucket(svc, name), "listBucket")
 }
